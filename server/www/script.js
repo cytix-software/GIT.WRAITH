dbfa5cd3388d9fa73aadf35f1fe7daa302a283ef
Amy <amy@cytix.io>
2025-04-09 16:45:52 +0100

    Make demo page actually work

--- server/www/script.js
+++ server/www/script.js
@@ -1,16 +1,78 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const glitchText = document.getElementById('glitch');
+  const glitchText = document.getElementById("glitch");
   const glitchTextContent = glitchText.textContent;
-  
+
   // Function to create random glitch effect on text
   function randomizeText() {
-      let randomText = glitchTextContent.split('').map(char => {
-          // Randomly convert characters to numbers or symbols to mimic a glitch
-          return Math.random() < 0.1 ? String.fromCharCode(Math.floor(Math.random() * 94) + 33) : char;
-      }).join('');
-      
-      glitchText.textContent = randomText;
+    let randomText = glitchTextContent
+      .split("")
+      .map((char) => {
+        // Randomly convert characters to numbers or symbols to mimic a glitch
+        return Math.random() < 0.1
+          ? String.fromCharCode(Math.floor(Math.random() * 94) + 33)
+          : char;
+      })
+      .join("");
+
+    glitchText.textContent = randomText;
   }
 
-  setInterval(randomizeText, 150);
+  setInterval(randomizeText, 200);
 });
+
+const demoButton = document.getElementById("demo-btn");
+const gitURL = document.getElementById("git-url");
+
+// Send github URL to Wraith server when demo button clicked
+demoButton.addEventListener("click", async (e) => {
+  if (!gitURL.value) return;
+
+  // Make a loading animation appear or inform the user stuff is happening
+
+  const res = await fetch("/api/scan?repo_url=" + gitURL.value);
+
+  if (!res.ok) {
+    return error(`Request failed: ${res.status} ${res.statusText}`);
+  }
+
+  let out;
+  try {
+    out = await res.json();
+  } catch {
+    return error("Failed to parse JSON data.");
+  }
+
+  const markdownOutput = out.reduce((str, [fileName, content]) => str += `# ${fileName}\n${content}\n\n`, '')
+  console.log(markdownOutput)
+  demoOutput(markdownOutput);
+});
+
+const outputBox = document.getElementById("demo-success");
+
+/** Make the demo output box visible and enter markdown into it */
+function demoOutput(markdown) {
+  outputBox.style.display = "block";
+  outputBox.style.color = "#66ff66";
+
+  let url;
+  try {
+    url = new URL(gitURL.value);
+  } catch {
+    return error(`<p>Failed to generate results - Given URL is not valid.</p>`);
+  }
+
+  // Convert the markdown to HTML using `marked`
+  const htmlContent = marked.parse(markdown);
+
+  outputBox.innerHTML =
+    `<p><strong>Analysis Results for Repository: <span>${url.pathname.slice(1)}</span></strong></p>`
+    + htmlContent;
+}
+
+/** Display an error in the output box */
+function error(message) {
+  outputBox.style.display = "block";
+  outputBox.style.color = "red";
+
+  outputBox.innerHTML = `<p>${message}</p>`;
+}
