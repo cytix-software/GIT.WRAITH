d7cd5c209f1c8129ce5f2f26d2abb9eb555cee10
Amy <amy@cytix.io>
2025-04-10 11:40:19 +0100

    Introduces diagram view
    Adds a dedicated page to view the system dataflow diagram.
    
    This change removes the diagram from the main analysis results and provides a link to a separate page for viewing the diagram. This improves readability and provides a better user experience for viewing the diagram.

--- server/serveStatic.py
+++ server/serveStatic.py
@@ -1,4 +1,4 @@
-from bottle import route, static_file
+from bottle import route, static_file, get, response
 import os
 
 ROOT = './server/www'
@@ -10,3 +10,17 @@ def serveStatic(filepath=''):
     if filepath == '' or filepath.endswith('/'):
         filepath = os.path.join(filepath, 'index.html')
     return static_file(filepath, root=ROOT)
+
+@get('/diagram')
+def renderDiagram():
+    try:
+        with open('./.git_wraith_repo/wraith.docs/system-dataflow.md', 'r') as f:
+            mermaidStr = f.read()
+        with open('./server/diagram.html', 'r') as f:
+            html = f.read()
+        
+        return html.replace('%mermaidStr%', mermaidStr)
+    except Exception as e:
+        response.status = 500
+        print(e)
+        return { 'error': f'Failed to read diagram file: {str(e)}' }
